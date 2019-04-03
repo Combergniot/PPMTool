1bb73d98c4b4d0e5588c815c0603a4ba01ae2c6c
Combergniot <marcin.wernerowicz@gmail.com>
2019-04-03 10:15:54 +0200

    findProjectTask with validation

--- src/main/java/com/combergniot/ppmtool/controllers/BacklogController.java
+++ src/main/java/com/combergniot/ppmtool/controllers/BacklogController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("api/backlog")
+@RequestMapping("/api/backlog")
 @CrossOrigin
 public class BacklogController {
 
@@ -36,4 +36,9 @@ public class BacklogController {
         return (projectTaskService.findBacklogById(backlog_id));
     }
 
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
 }
